6d39d7d483d6974094876dc25a9bc546ab2b0972
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-02 17:38:11 +0530

    Added. Flip Coin Program

---
+++ src/main/java/com/bdlz/BC/FlipCoin.java
@@ -0,0 +1,13 @@
+package com.bdlz.BC;
+
+public class FlipCoin {
+    public static void main(String[] args) {
+        double coin = (int) (Math.random() * 10) % 2;
+        if (coin == 1) {
+            System.out.println("Head");
+        } else {
+            System.out.println("Tail");
+        }
+    }
+
+}
